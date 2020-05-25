58a7f9a203127f44b001a1b00726b86415583aa2
Slava142192 <slava1421@gmail.com>
2020-05-25 19:15:15 +0300

    create app

---
+++ src/modules/page-camera/components/page-camera/page-camera.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-camera',
+  templateUrl: './page-camera.component.html',
+  styleUrls: ['./page-camera.component.scss']
+})
+export class PageCameraComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/modules/page-camera/page-camera.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PageCameraComponent } from './components/page-camera/page-camera.component';
+
+
+
+@NgModule({
+  declarations: [PageCameraComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class PageCameraModule { }
